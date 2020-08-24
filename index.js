866486ef6f2466f9cebc2202630e419365162c0d
pheno <nicolas.enault@gmail.com>
2020-08-24 17:07:03 +0200

    modifs js

---
+++ index.js
@@ -0,0 +1,24 @@
+let teamMember = {
+    raphaelle: {
+        who: 'Lorem Ipsum',
+        what: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        where: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        when:'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        how:'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+      },
+    nicolas: {
+        who: 'Lorem Ipsum',
+        what: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        where: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        when:'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        how:'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    },
+  };
+
+  <div id="raphaelle" class="teamMember">
+    <div id="who"></div>
+    <div id="what"></div>
+    <div id="where"></div>
+    <div id="when"></div>
+    <div id="how"></div>
+</div>
